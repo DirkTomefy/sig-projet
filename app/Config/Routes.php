13cd097301e2a24d55cdcb5ac1c7916eadf76396
YvNathan <ratefinjanaharynathan@gmail.com>
2026-07-03 23:20:31 +0300

    [feat]: controller + views + script + routing

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -4,3 +4,13 @@ use CodeIgniter\Router\RouteCollection;
 
 /** @var RouteCollection $routes */
 $routes->get('/', 'Home::index');
+
+$routes->group('api/statistiques', static function ($routes) {
+    $routes->get('dashboard', 'StatistiqueController::dashboard');
+    $routes->get('resume', 'StatistiqueController::resume');
+    $routes->get('etablissements-par-type', 'StatistiqueController::etablissementsParType');
+    $routes->get('etablissements-par-arrondissement', 'StatistiqueController::etablissementsParArrondissement');
+    $routes->get('couverture-par-arrondissement', 'StatistiqueController::couvertureParArrondissement');
+    $routes->get('repartition-type-arrondissement', 'StatistiqueController::repartitionTypeParArrondissement');
+    $routes->get('annees-recensement', 'StatistiqueController::anneesRecensement');
+});
